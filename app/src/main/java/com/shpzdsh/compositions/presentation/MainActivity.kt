ffa0a7c94787e8128d85e5fa2f0ee4a28acb0fa1
Valentin Sidorov <168553669+Shpzdsh@users.noreply.github.com>
2025-07-25 21:06:04 +0400

    add domain and data.GameRepositoryImpl

---
+++ app/src/main/java/com/shpzdsh/compositions/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shpzdsh.compositions.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shpzdsh.compositions.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
