b760ba867f0d78a777b710e0c44165eb7e0867da
Valentin Sidorov <168553669+Shpzdsh@users.noreply.github.com>
2025-07-31 22:05:44 +0400

    add five fragments and resource files for fragments

---
+++ app/src/main/java/com/shpzdsh/compositions/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,20 @@
+package com.shpzdsh.compositions.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.shpzdsh.compositions.R
+
+
+class ChooseLevelFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    }
+}
