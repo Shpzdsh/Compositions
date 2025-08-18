892f76c20da4a4446025135f9e43d0d272c107ab
Valentin Sidorov <168553669+Shpzdsh@users.noreply.github.com>
2025-08-18 23:59:34 +0400

    add Jetpack Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.safeargs)
 
 }
 
@@ -54,6 +55,9 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
 
     implementation(libs.androidx.material)
 
